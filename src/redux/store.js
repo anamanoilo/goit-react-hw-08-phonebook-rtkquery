df3576b2e3dae06047a686e80a8cdcfd67a9933b
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-05-02 16:29:17 +0200

    initial commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import rootReducer from './phonebook/phonebook-reducers';
-
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  blacklist: ['filter'],
-};
 
 const store = configureStore({
   reducer: {
-    phonebook: persistReducer(contactsPersistConfig, rootReducer),
+    phonebook: rootReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
   devTools: process.env.NODE_ENV === 'development',
 });
 
-const persistedStore = persistStore(store);
-const unitedStores = { persistedStore, store };
-
-export default unitedStores;
+export default store;
